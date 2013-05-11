a8534bba8384d5b9364404e925fefd8dddc632c4
Christopher Eyre <chriseyre2000@gmail.com>
2013-05-11 20:39:32 +0100

    Initial version
    This is a demo Azure Log Viewer App

---
+++ AzureLoggingDisplay/LogViewerWebRole/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using LogViewerWebRole.Models;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LogViewerWebRole.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            Trace.TraceWarning("Index has been called...");
+            ILogEntries model = LogsInTheLastHour();
+            return View(model);
+        }
+
+        public ActionResult LastHour()
+        {
+            Trace.TraceWarning("LastHour has been called...");
+            ILogEntries model = LogsInTheLastHour();
+            return View("Index", model);
+        }
+
+        private static ILogEntries LogsInTheLastHour()
+        {
+            DateTime keepThreshold = DateTime.UtcNow.AddHours(-1);            
+            string filter = string.Format("PartitionKey gt '0{0}'", keepThreshold.Ticks);
+            return Logs(filter);
+        }
+
+        private static ILogEntries Logs(string filter)
+        {
+            string diagnostics = RoleEnvironment.GetConfigurationSettingValue("Microsoft.WindowsAzure.Plugins.Diagnostics.ConnectionString");
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(diagnostics);
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+            CloudTable cloudTable = tableClient.GetTableReference("WADLogsTable");
+            TableQuery query = new TableQuery();
+
+            query.FilterString = filter;
+            var items = cloudTable.ExecuteQuery(query).ToList();
+
+            ILogEntries model = new LogEntries();
+
+            foreach (var item in items)
+            {
+                model.Log.Insert(0, new LogItem
+                {
+                    Level = item.Properties["Level"].Int32Value ?? 0,
+                    RoleInstance = item.Properties["RoleInstance"].StringValue,
+                    Message = item.Properties["Message"].StringValue,
+                    PartitionKey = item.PartitionKey,
+                    Timestamp = item.Timestamp,
+                });
+            }
+            return model;
+        }
+
+    }
+}
---
+++ AzureLoggingDisplay/LogViewerWebRole/Models/LogEntries.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LogViewerWebRole.Models
+{
+    public class LogItem
+    {
+        public int Level { get; set; }
+        public string RoleInstance { get; set; }
+        public string Message { get; set; }
+        public string PartitionKey { get; set; }
+        public DateTimeOffset Timestamp { get; set; }
+    }
+
+    public interface ILogEntries 
+    {
+        List<LogItem> Log { get; set; }
+    }
+
+    public class LogEntries : ILogEntries
+    {
+        public LogEntries() 
+        {
+            Log = new List<LogItem>();
+        }
+
+        public List<LogItem> Log { get; set; }
+    }
+}
